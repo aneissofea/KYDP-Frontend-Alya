ce46d836068bfee8f7987a341bdc3177425fec09
Alya Mohd <alya.mohd96@gmail.com>
2024-07-06 10:52:57 +0800

    inserted button clear

--- Practicuum/news-app/src/components/MyFavouritesPanel.jsx
+++ Practicuum/news-app/src/components/MyFavouritesPanel.jsx
@@ -1,12 +1,20 @@
-import { Grid } from "@mui/material";
+import { Grid, Button } from "@mui/material";
+import { useState } from "react";
 
 function MyFavouritesPanel() {
 
+    const [favouriteslist, setFavouriteslist] = useState({});
 
+    const handleClear = () => {
+        
+    };
 
     return(
         <Grid container direction='row' style={{display: "flex", backgroundColor: "#698474" }}>
             <h3>My Favourites:</h3>
+            <Button variant="contained" onClick={handleClear}>
+                Search
+            </Button>
         </Grid>
         
     );
