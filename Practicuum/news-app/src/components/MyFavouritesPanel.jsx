391ad034285bb44f862a35922608056b762a6142
Alya Mohd <alya.mohd96@gmail.com>
2024-07-06 11:03:46 +0800

    adding clear button function handler

--- Practicuum/news-app/src/components/MyFavouritesPanel.jsx
+++ Practicuum/news-app/src/components/MyFavouritesPanel.jsx
@@ -3,17 +3,17 @@ import { useState } from "react";
 
 function MyFavouritesPanel() {
 
-    const [favouriteslist, setFavouriteslist] = useState({});
+    const [myFavourites, setFavourites] = useState({});
 
     const handleClear = () => {
-        
+      setFavourites({""})  
     };
 
     return(
         <Grid container direction='row' style={{display: "flex", backgroundColor: "#698474" }}>
             <h3>My Favourites:</h3>
             <Button variant="contained" onClick={handleClear}>
-                Search
+                Clear
             </Button>
         </Grid>
         
